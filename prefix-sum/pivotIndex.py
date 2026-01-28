288fe873dceabd265aba77f104cb96f76adeb83d
blip-cmd <niiakweibrown1@gmail.com>
2026-01-28 12:51:26 +0000

    code up

--- prefix-sum/pivotIndex.py
+++ prefix-sum/pivotIndex.py
@@ -11,4 +11,11 @@ class Solution:
         # is this efficient enough?
         # or i should think of a better approach.
 
-        
+        total_sum = sum(nums)
+        left_sum= 0
+        for i in range(len(nums)):
+            right_sum = total_sum-left_sum-nums[i]
+            if left_sum==right_sum:
+                return i
+            left_sum += nums[i]
+        return -1
