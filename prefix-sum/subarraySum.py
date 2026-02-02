c671d9bdee6796ea08188b22b2569472852eff38
blip-cmd <niiakweibrown1@gmail.com>
2026-02-02 14:45:51 +0000

    optimize with prefix_dict

--- prefix-sum/subarraySum.py
+++ prefix-sum/subarraySum.py
@@ -6,17 +6,28 @@ class Solution(object):
         :rtype: int
         """
 
-        #brute force 
+        # brute force 
         # check each possible subarray and sum. and count sums matching given k
 
+        # result = 0
+        
+        # for i in range(len(nums)):
+        #     total = 0
+        #     for j in range(i,len(nums)):
+        #         total += nums[j]
+        #         if total == k:
+        #             result += 1
+
+        # return result
+        
+        #optimized using hashmap to store cumulative sum frequencies
         result = 0
-        
+        prefix_dict = {0:1}
+        running_sum = 0 
         for i in range(len(nums)):
-            total = 0
-            for j in range(i,len(nums)):
-                total += nums[j]
-                if total == k:
-                    result += 1
-                    
-        return result
+            running_sum += nums[i]
+            dif = running_sum - k
+            result += prefix_dict.get(dif,0)
+            prefix_dict[running_sum] = 1 + prefix_dict.get(running_sum, 0) 
         
+        return result
