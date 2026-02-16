04a9d55d18a750eb1240f3f90a7f07a364190fb5
blip-cmd <niiakweibrown1@gmail.com>
2026-02-16 09:47:37 +0000

    working code

--- random/insert-interval.py
+++ random/insert-interval.py
@@ -22,8 +22,27 @@ class Solution(object):
     # no overlaps left, append the remaining pairs.
     # res = [1,2],[3,10],[12,16]
 
-        
-    #python
-    
+        #python
 
+        result = []
+        i = 0
+        n = len(intervals)
+
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
         
+        while i < n and intervals[i][0] <= newInterval[1] :
+            newInterval[0] = min(intervals[i][0],newInterval[0])
+            newInterval[1] = max(intervals[i][1],newInterval[1])
+            
+            i += 1
+            
+        result.append(newInterval)
+
+        while i<n:
+            result.append(intervals[i])
+            i += 1
+
+        return result
+       
